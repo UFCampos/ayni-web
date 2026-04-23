8ee4e670f47f2825617b570ccdd71764cd6dc8b7
UrielC2004 <urielcampos448@gmail.com>
2026-04-23 14:16:05 -0300

    Fix conditional rendering for ClaimSteps component

--- app/page.tsx
+++ app/page.tsx
@@ -41,7 +41,7 @@ const Home = (): React.JSX.Element => {
       <section className='p-2 my-4 z-10 w-full '>
         <div className='flex xl:grid grid-cols-3 flex-col place-items-start items-center gap-4 w-full' >
           <InfoPresale />
-            {endsale
+            {true
               ? <ClaimSteps />
               : (
             <section className='bg-background/50 self-center border border-border-secondary rounded-3xl xl:-ml-20 flex flex-col mx-0 w-full md:w-[500px] xl:w-11/12 py-4'>
